53a2acde2cd01fa4da7e0bc4839e39d204cc4d62
lMyBadl <yd89782@gmail.com>
2025-01-21 12:10:23 -0600

    Update shengJi.py

--- shengJi.py
+++ shengJi.py
@@ -9,6 +9,7 @@ player2 = Player("Player 2")
 player3 = Player("Player 3")
 player4 = Player("Player 4")
 
+#draws until deck is empty, and if empty then returns false
 while deck.cards:
     player1.draw(deck)
     player2.draw(deck)
