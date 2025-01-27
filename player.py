dffba0db986f86854d3766aacd3831900044c55d
amogk_ <amogkpatil@gmail.com>
2025-01-27 13:46:35 -0600

    oof

--- player.py
+++
@@ -1,9 +0,0 @@
-from deck import Deck
-class Player:
-    def __init__(self, name):
-        self.name = name
-
-    def drawCard(self, deck):
-        card = deck.drawCard()
-        if card:
-            self.hand.append(card)
