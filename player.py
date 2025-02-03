b65249618d2b9564f4bc462983f0746575e6f584
lMyBadl <yd89782@gmail.com>
2025-02-03 14:23:29 -0600

    Changes by Amogk

--- player.py
+++ player.py
@@ -6,3 +6,15 @@ class Player:
         
     def drawCard(self, deck):
         self.hand.append(deck.drawCard())
+
+    def playCard(self, card):
+        self.hand.remove(card)
+
+    def getHand(self):
+        return self.hand
+
+    def __str__(self):
+        output = ""
+        for card in self.hand:
+            output += str(card) + " "
+        return output
